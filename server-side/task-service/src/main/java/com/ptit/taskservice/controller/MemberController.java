c245a8f81cafec14a5c9aabfed3babafe9ebed6a
dodt1306 <dodo.9x@gmail.com>
2025-08-13 11:35:04 +0700

    Update MemberController.java

--- server-side/task-service/src/main/java/com/ptit/taskservice/controller/MemberController.java
+++ server-side/task-service/src/main/java/com/ptit/taskservice/controller/MemberController.java
@@ -24,7 +24,7 @@ public class MemberController {
 
   @GetMapping
   public ResponseEntity<List<MemberResponse>> findAll() {
-    return ResponseEntity.ok(service.findAll());
+    return ResponseEntity.ok("cccc");
   }
 
 }
